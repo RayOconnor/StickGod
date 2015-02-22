1da9edc0233936044b068649efb5d5ed27281be2
brandenrodgers <branden@Brandens-MacBook-Pro.local>
2015-02-22 17:03:00 -0500

    added spawn and collision

--- game.js
+++ game.js
@@ -23,7 +23,7 @@ var materials;
 // def = int defense 
 // atk = int attack
 // rng = int range
-function stickMan(x, y, z, speed, health, def, atk, rng){
+function stickMan(x, y, z, speed, health, def, atk, rng, fight){
 	this.x = x;
 	this.y = y;
 	this.z = z;
@@ -32,6 +32,7 @@ function stickMan(x, y, z, speed, health, def, atk, rng){
 	this.def = def;
 	this.atk = atk;
 	this.rng = rng;
+	this.fight = fight;
 }
 
 //Player Army
@@ -55,17 +56,77 @@ if (document.images)
 {
 	var img = new Array();
 	img[0] = new Image();
-	img[0].src = 'stickman.png';
+	img[0].src = 'stickmanside.png';
+	img[1] = new Image();
+	img[1].src = 'stickmantop.png';
 }
 
 
-//
-function move_soldiers()
+//moves the soldiers in the player army
+function move_player_soldiers()
 {
 	for (var i = 0; i < pArmy.pFootSoldiers.length; i++)
 	{
 		pArmy.pFootSoldiers[i].x += pArmy.pFootSoldiers[i].speed;
 	}
+}
+
+//Spawns player soldiers in player army
+function spawn_player_soldiers(ypos)
+{
+	var zpos = ypos;
+	var soldier = new stickMan(0, ypos, zpos, 2, 10, 10, 5, 2, false);
+	pArmy.pFootSoldiers.push(soldier);
+}
+
+//Moves the soldiers in the enemy army
+function move_enemy_soldiers()
+{
+	for (var i = 0; i < eArmy.eFootSoldiers.length; i++)
+	{
+		eArmy.eFootSoldiers[i].x -= eArmy.eFootSoldiers[i].speed;
+	}
+}
+
+function check_collision()
+{
+	for (var i = 0; i < pArmy.pFootSoldiers.length; i++)
+	{
+		for (var e = 0; e < eArmy.eFootSoldiers.length; e++)
+		{
+			if (pArmy.pFootSoldiers[i].y == eArmy.eFootSoldiers[e].y &&
+				Math.abs(pArmy.pFootSoldiers[i].x - eArmy.eFootSoldiers[e].x) < 20)
+			{
+				pArmy.pFootSoldiers[i].speed = 0;
+				eArmy.eFootSoldiers[e].speed = 0;
+			}
+		}
+	}
+}
+
+
+//Spawns player soldier at click location
+function keyListener(e) {
+    if(!e)
+	{
+		e = window.event;
+	}
+    var yPos = e.clientY;
+    
+    if (yPos <= 250)
+    {
+    	yPos = 100;
+    }
+    else if (yPos > 250 && yPos <= 400)
+    {
+    	yPos = 200;
+    }
+    else if (yPos > 400)
+    {
+    	yPos = 300;
+    }
+    spawn_player_soldiers(yPos);
+
 }
 
 
@@ -78,8 +139,15 @@ function main_draw()
 	for (var i = 0; i < pArmy.pFootSoldiers.length; i++)
 	{
 		var stick_image = new Image();
-		stick_image.src = "stickman.png";
+		stick_image.src = "stickmanside.png";
 		context.drawImage(stick_image,pArmy.pFootSoldiers[i].x,pArmy.pFootSoldiers[i].y);
+	}
+
+	for (var i = 0; i < eArmy.eFootSoldiers.length; i++)
+	{
+		var enemy_image = new Image();
+		enemy_image.src = "stickmanside.png";
+		context.drawImage(enemy_image,eArmy.eFootSoldiers[i].x,eArmy.eFootSoldiers[i].y);
 	}
 	
 }
@@ -90,9 +158,19 @@ function god_draw()
 	var context = god_canvas.getContext("2d");
 	context.clearRect(0,0,XMax,ZMax);
 	
-	var stick_image = new Image();
-	stick_image.src = "stickman.png";
-	context.drawImage(stick_image,100,100);
+	for (var i = 0; i < pArmy.pFootSoldiers.length; i++)
+	{
+		var stick_image = new Image();
+		stick_image.src = "stickmantop.png";
+		context.drawImage(stick_image,pArmy.pFootSoldiers[i].x,pArmy.pFootSoldiers[i].z);
+	}
+
+	for (var i = 0; i < eArmy.eFootSoldiers.length; i++)
+	{
+		var enemy_image = new Image();
+		enemy_image.src = "stickmantop.png";
+		context.drawImage(enemy_image,eArmy.eFootSoldiers[i].x,eArmy.eFootSoldiers[i].z);
+	}
 }
 
 function draw_all()
@@ -107,8 +185,11 @@ function draw_all()
 //Waits for step
 function step()
 {
-	move_soldiers();
 	draw_all();
+	move_player_soldiers();
+	move_enemy_soldiers();
+	check_collision();
+	
 	if(pFortHealth <= 0)
 	{
 		document.getElementById("result").innerHTML = "GAME OVER";
@@ -131,9 +212,14 @@ function wait_for_step()
 function game()
 {
 	document.getElementById("button").style.visibility = 'hidden';
-	var soldier1 = new stickMan(50, 100, 50, 5, 10, 10, 5, 2);
-	var soldier2 = new stickMan(50, 200, 50, 5, 10, 10, 5, 2);
+	document.getElementById("main_screen").onmousedown = keyListener;
+	var soldier1 = new stickMan(50, 100, 100, 5, 10, 10, 5, 2, false);
+	var soldier2 = new stickMan(20, 200, 200, 2, 10, 10, 5, 2, false);
+	var enemy1 = new stickMan(350, 100, 100, 5, 10, 10, 5, 2, false);
+	var enemy2 = new stickMan(380, 200, 200, 2, 10, 10, 5, 2, false);
 	pArmy.pFootSoldiers.push(soldier1);
 	pArmy.pFootSoldiers.push(soldier2);
+	eArmy.eFootSoldiers.push(enemy1);
+	eArmy.eFootSoldiers.push(enemy2);
 	wait_for_step();
 }
